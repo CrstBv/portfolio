f18137a2e942a9f5c9d9b5b812eda079ffe2e794
CristianB <angelC.bV@outlpook.com>
2024-08-29 00:58:16 -0600

    Format components and updated files

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,7 +5,7 @@ import { email } from "@/constants";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 export default function Home() {
-  const t = useTranslations()
+  const t = useTranslations();
   return (
     <div className="container relative">
       <div className="max-w-7xl lg:mx-6 p-6 md:px-4 xl:px-0 w-full my-12 flex flex-col justify-center gap-8 md:gap-12">
@@ -13,13 +13,17 @@ export default function Home() {
           <div id="about">
             <div className="flex flex-col md:flex-row gap-8">
               <div>
-                <h2 className="text-xl pl-32 pt-20 gap-8">{t("about-section.title")}</h2>
+                <h2 className="text-xl pl-32 pt-20 gap-8">
+                  {t("about-section.title")}
+                </h2>
                 <p className="flex flex-col px-8 max-w-[680px] text-justify">
                   {t("about-section.description")}{" "}
                 </p>
                 <div className="pl-[102px] pt-6">
                   <a href={t("about-section.href-CV")} download>
-                    <Button variant="outline">{t("about-section.DownloadButton")}</Button>
+                    <Button variant="outline">
+                      {t("about-section.DownloadButton")}
+                    </Button>
                   </a>
                 </div>
               </div>
@@ -37,7 +41,9 @@ export default function Home() {
         </div>
         <div className="py-5 min-h-[350px]">
           <div>
-            <h2 className="flex justify-center text-lg">{t("skills-section.title")}</h2>
+            <h2 className="flex justify-center text-lg">
+              {t("skills-section.title")}
+            </h2>
             <Skills />
             <p></p>
           </div>
@@ -46,7 +52,7 @@ export default function Home() {
           <div className="flex justify-center w-full py-4 md:py-10 md:pb-8 xl:py-18 xl:pb-16">
             <div className="flex flex-col items-center gap-10">
               <span className="pt-12 text-[20px] text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-teal-200">
-              {t("projects-section.title")}
+                {t("projects-section.title")}
               </span>
               <ProjectCard />
             </div>
@@ -55,25 +61,25 @@ export default function Home() {
         <div id="contact">
           <div className="flex justify-center min-h-[280px] flex-col items-center">
             <h2 className="pt-16 pb-4 text-xl">{t("contact-section.title")}</h2>
-              <p className="text-md">
-                {t("contact-section.email")}
-                <a className="pl-2 hover:text-blue-400" href={`mailto:${email}`}>
-                  {email}
-                </a>
-              </p>
-              <a
-                href="https://github.com/CrstBv"
-                className="flex items-center text-blue-200"
-              >
-                <Image
-                  src={"/assets/icons/github.svg"}
-                  width={20}
-                  height={20}
-                  alt="contact"
-                  className="bg-blue-100"
-                />
-                Github
+            <p className="text-md">
+              {t("contact-section.email")}
+              <a className="pl-2 hover:text-blue-400" href={`mailto:${email}`}>
+                {email}
               </a>
+            </p>
+            <a
+              href="https://github.com/CrstBv"
+              className="flex items-center text-blue-200"
+            >
+              <Image
+                src={"/assets/icons/github.svg"}
+                width={20}
+                height={20}
+                alt="contact"
+                className="bg-blue-100"
+              />
+              Github
+            </a>
           </div>
         </div>
       </div>
--- components/home/Skills.tsx
+++ components/home/Skills.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import SkillDataProvider from './SkillsDataProvider'
-import { skillData } from '@/constants'
+import React from "react";
+import SkillDataProvider from "./SkillsDataProvider";
+import { skillData } from "@/constants";
 
 const Skills = () => {
   return (
-    <section className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'
-    style={{transform: "scale(0.9"}}
+    <section
+      className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center"
+      style={{ transform: "scale(0.9" }}
     >
-        <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {skillData.map((image, index) => (
-           <SkillDataProvider 
-        key={index}
-        src={image.Image}
-        width={image.width}
-        height={image.height} 
-        index={index}
-        />
+          <SkillDataProvider
+            key={index}
+            src={image.Image}
+            width={image.width}
+            height={image.height}
+            index={index}
+          />
         ))}
-    </div>
+      </div>
     </section>
-    
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
